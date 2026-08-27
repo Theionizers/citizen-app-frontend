175bc906c737f923d3e2e4269e21b70b3e6b3b48
Rachit Verma <rachitverma539@gmail.com>
2026-08-27 17:14:49 +0530

    SubmitComplaint ui started

---
+++ src/pages/SubmitComplaint.jsx
@@ -0,0 +1,8 @@
+const SubmitComplaint = () => {
+  return (
+    <div className="min-h-screen bg-[#FFF8F1]">
+      <Navbar />
+
+      <main className="pt-32 pb-16 px-5 sm:px-8 lg:px-10">
+        <div className="max-w-4xl mx-auto">
+    }
